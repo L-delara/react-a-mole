ae024bdd12b97c9b79ef6de21079510d19e97764
L-delara <ligia.delara15@gmail.com>
2023-06-01 18:34:29 -0400

    Finish

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { useState } from "react";
+import MoleContainer from "./Components/MoleContainer";
 
 function App() {
-  return <div></div>;
+  let [score, setScore] = useState(0);
+
+  const createMoleHill = () => {
+    let hills = [];
+    for (let i = 0; i < 9; i++) {
+      hills.push(
+        <MoleContainer key={i} setScore={setScore} score={score} />
+      );
+    }
+    return <div>{hills}</div>;
+  };
+
+  return (
+    <div className="App">
+      <h1>React-A-Mole!</h1>
+      <h2>{score}</h2>
+      {createMoleHill()}
+    </div>
+  );
 }
 
 export default App;
